95d893c6d63272b142c18f62b4fc46ee8167077a
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-11 05:22:15 +0800

    First commit. Created the header with shadow bottom.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Home/Header'
+import Hero from './components/Home/Hero'
+import './App.scss'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Hero/>
+      </div> 
+    )
+  }
+}
+
+export default App;
