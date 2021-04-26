5e7b8216a444cb92dd5ef2783f7b70e02e46ca76
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-27 03:11:44 +0800

    Back to bootstrap form.

--- src/components/Home/OptForm.js
+++ src/components/Home/OptForm.js
@@ -1,43 +1,16 @@
 import React from 'react'
-import chevronRight from '../../assets/images/chevron-right.png'
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const OptForm = ({ children, ...restProps}) => {
+const OptForm = () => {
   return (
-    <Container { ...restProps}> {children} </Container>
+    <StyledOptForm>
       
+    </StyledOptForm>
   )
 }
+
+const StyledOptForm = styled.div`
+  
+`
 
 export default OptForm
-
-OptForm.Input = function OptFormInput({ ...restProps}) {
-  return <Input { ...restProps} />
-}
-
-OptForm.Button = function OptFormButton({ children, ...restProps}) {
-  return (
-    <Button { ...restProps}>
-      {children} <img src={chevronRight} alt='Try Now' />
-    </Button>
-  )
-}
-
-OptForm.Text = function OptFormText({ children, ...restProps}) {
-  return <Text { ...restProps}>{children}</Text>
-}
-
-const Container = styled.div`
-
-`
-const Input = styled.input`
-  
-`
-
-const Button = styled.button`
-  
-`
-
-const Text = styled.p`
-  
-`
--- src/components/Home/StoryCard.js
+++ src/components/Home/StoryCard.js
@@ -17,11 +17,7 @@ const Header = () => {
 					Ready to learn? Enter your email to start your{' '}
 					<span>30-day free trial.</span>
 				</h3>
-				<OptForm>
-          <OptForm.Input placeholder='Email address'/>
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
-        </OptForm>
+				<OptForm/>
 			</div>
 		</StyledStoryCard>
 	)
