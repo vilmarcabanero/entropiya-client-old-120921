2d22d32f2a2522db7b50232e2f23a87e1be705c2
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-29 01:46:41 +0800

    tried i-position absolute lang ang button. But failed kasi dapat magka tabi lang sila ng input, not overlapping

--- src/components/Home/OptForm.js
+++ src/components/Home/OptForm.js
@@ -15,11 +15,25 @@ const OptForm = () => {
 							required=''
 							placeholder='Email address'
 						/>
+
 						<div class='input-group-append'>
 							<button type='submit' class='submit'>
 								<span>
 									Enroll now <FaAngleDownRight />
 								</span>
+							</button>
+						</div>
+					</div>
+					<div class='input-button-group'>
+						<label>
+							<input type='text' class='form-control' id='' required />
+
+							<span className='placeholder'>Email address</span>
+						</label>
+
+						<div class='button-container'>
+							<button type='submit' class='submit'>
+								Enroll now <FaAngleDownRight className='arrow' />
 							</button>
 						</div>
 					</div>
@@ -31,17 +45,12 @@ const OptForm = () => {
 
 const favBlueBackground = '#4398ff'
 
-const FaAngleDownRight = styled(FaAngleDown)`
-	font-size: 1.25rem;
-	color: white;
-	transform-origin: center center;
-	transform: rotate(-90deg);
-`
+const FaAngleDownRight = styled(FaAngleDown)``
 
 const StyledOptForm = styled.div`
 	.form-section form {
 		position: relative;
-    margin-top: 1rem;
+		margin-top: 1rem;
 		.input-group {
 			input {
 				height: 50px;
@@ -52,12 +61,12 @@ const StyledOptForm = styled.div`
 					font-size: 1.1rem;
 				}
 
-        @media (max-width: 30rem) {
-          height: 35px;
-          font-size: 0.95rem;
-					&::placeholder {
+				@media (max-width: 30rem) {
+					height: 35px;
 					font-size: 0.95rem;
-				}
+					&::placeholder {
+						font-size: 0.95rem;
+					}
 				}
 			}
 
@@ -77,7 +86,7 @@ const StyledOptForm = styled.div`
 
 				@media (max-width: 30rem) {
 					width: 130px;
-          height: 35px;
+					height: 35px;
 				}
 
 				> span {
@@ -98,19 +107,113 @@ const StyledOptForm = styled.div`
 						&:hover {
 							font-size: 1.35rem;
 						}
-            top: 49.5%;
+						top: 49.5%;
 					}
 
 					@media (max-width: 30rem) {
 						width: 120px;
 						font-size: 1rem;
-            height: 32px;
-            line-height: 32px;
+						height: 32px;
+						line-height: 32px;
 						&:hover {
 							font-size: 0.9rem;
 						}
 					}
 				}
+			}
+		}
+	}
+
+	.input-button-group {
+		display: flex;
+		margin-top: 2rem;
+
+		label {
+			position: relative;
+			width: 100%;
+			input {
+				height: 3.5rem;
+				color: black;
+				font-size: 1rem;
+				outline: none;
+				border: none;
+				padding-bottom: 0px;
+				&:valid + .placeholder,
+				&:focus + .placeholder {
+					top: 0px;
+					font-size: 0.75rem;
+					color: black;
+				}
+
+				@media (max-width: 30rem) {
+					height: 3rem;
+					font-size: 0.95rem;
+				}
+			}
+
+			.placeholder {
+				font-size: 1rem;
+				position: absolute;
+				left: 0.75rem;
+				top: 1rem;
+				color: #aaa;
+				transition: top 0.3s ease, font-size 0.3s ease, color 0.3s ease;
+				cursor: text;
+				@media (max-width: 30rem) {
+					top: 0.8rem;
+					font-size: 0.95rem;
+				}
+			}
+		}
+
+		.button-container {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: white;
+			height: 
+		}
+
+		button {
+			color: white;
+			text-align: center;
+			cursor: pointer;
+			// width: 140px;
+			width: 10.5rem;
+			padding: 0px 8px;
+			border: none;
+			outline: none;
+			background-color: #06f;
+			border-radius: 4px;
+			height: 3rem;
+
+			font-size: 1.3rem;
+
+			@media (max-width: 30rem) {
+				width: 7.75rem;
+				height: 2.5rem;
+				font-size: 1rem;
+				.arrow {
+					height: 1rem;
+				}
+			}
+
+			@media (min-width: 30rem) {
+				&:hover {
+					background-color: transparent;
+					border: 1px solid #06f;
+					color: #06f;
+					.arrow {
+						color: #06f;
+					}
+				}
+			}
+
+			.arrow {
+				font-size: 1.25rem;
+				color: white;
+				transform-origin: center center;
+				transform: rotate(-90deg);
 			}
 		}
 	}
