740848682f0e862303596de74b4233dd84cde61a
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-03 07:17:29 +0800

    Binalik to https://entropiya-server.herokuapp.com/ ang baseURL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: `https://entropiya-server.herokuapp.com/`
-  baseURL: `http://localhost:5000/`
+  baseURL: `https://entropiya-server.herokuapp.com/`
+  // baseURL: `http://localhost:5000/`
 });
